1b8321824d28b42249dc7b89fc87cdeb986e83b2
Harshdip Saha <141698575+HARSHDIPSAHA@users.noreply.github.com>
2024-07-27 15:48:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,21 @@ similarity_scores = pickle.load(open('models/similarity_scores.pkl','rb'))
 def recommend(anime):
     # index fetch
     index = np.where(table.index == anime)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
     
     recommendations = {
         "Name": [],
-        "Other name": [],
         "Abstract": [],
         "Genre": []
     }
     
     for i, (anime_index, score) in enumerate(similar_items, start=1):
         temp_df = data[data['English name'] == table.index[anime_index]]
-        name = temp_df['Name'].drop_duplicates().values[0]
-        other_name = temp_df['Other name'].drop_duplicates().values[0]
+        name = temp_df['English name'].drop_duplicates().values[0]
         abstract = temp_df['Synopsis'].drop_duplicates().values[0]
         genres = temp_df['Genres'].drop_duplicates().values[0]
         
         recommendations["Name"].append(name)
-        recommendations["Other name"].append(other_name)
         recommendations["Abstract"].append(abstract)
         recommendations["Genre"].append(genres)
     
@@ -47,7 +44,6 @@ if st.button("Recommend"):
 
         for i in range(len(recommendations["Name"])):
             st.markdown(f"### {i+1}. {recommendations['Name'][i]}")
-            st.write(f"**Other name:** {recommendations['Other name'][i]}")
             st.write(f"**Abstract:** {recommendations['Abstract'][i]}")
             st.write(f"**Genre:** {recommendations['Genre'][i]}")
             st.write("---")
