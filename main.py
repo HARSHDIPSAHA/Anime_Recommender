563eebc50c398cbbd907b85bcded959d2ef6a967
Harshdip Saha <141698575+HARSHDIPSAHA@users.noreply.github.com>
2024-07-27 17:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def recommend(anime):
         other_name = temp_df['Other name'].drop_duplicates().values[0]
         abstract = temp_df['Synopsis'].drop_duplicates().values[0]
         genres = temp_df['Genres'].drop_duplicates().values[0]
-        image_url = temp_df['Image URL'].drop_duplicates().values[0]  # Assuming 'Image URL' is the column name
+        image_url = temp_df['Image URL'].drop_duplicates().values[0]
         
         recommendations["Name"].append(name)
         recommendations["Other name"].append(other_name)
@@ -63,7 +63,7 @@ if st.button("Recommend"):
                 <img src="{user_anime_image}" alt="{anime_input}">
             </div>
             <div class="user-info-container">
-                <h2>{anime_input}</h2>
+                <h4>{anime_input}❤️👏</h4>
             </div>
         </div>
         """, unsafe_allow_html=True)
@@ -76,7 +76,7 @@ if st.button("Recommend"):
                 </div>
                 <div class="info-container">
                     <h3>{i+1}. {recommendations['Name'][i]}</h3>
-                    <p><strong>Other name:</strong> {recommendations['Other name'][i]}</p>
+                    <p><strong>Japanese name:</strong> {recommendations['Other name'][i]}</p>
                     <p><strong>Abstract:</strong> {recommendations['Abstract'][i]}</p>
                     <p><strong>Genre:</strong> {recommendations['Genre'][i]}</p>
                 </div>
@@ -92,6 +92,6 @@ st.markdown("""
     <p>This Anime Recommendation System uses a dataset of anime titles, genres, and synopses to recommend similar anime based on your input.</p>
     <p>Rather than simply based on similar story and content, this system uses the data of various users and then give suggestions,<a href="https://developers.google.com/machine-learning/recommendation/collaborative/basics"> Collaborative filtering </a>
     <p><strong>Dataset Used:</strong> <a href="https://www.kaggle.com/datasets/dbdmobile/myanimelist-dataset">Kaggle Dataset</a></p>
-    <p><strong>Feedback:</strong> If you have any feedback or suggestions, please feel free to <a href="mailto:harshdipsaha95@gmail.com">email me</a>.</p>
+    <p><strong>Feedback:</strong> If you have any feedback or suggestions, please feel free to <a href="harshdipsaha95@gmail.com">email me</a>.</p>
 </div>
 """, unsafe_allow_html=True)
